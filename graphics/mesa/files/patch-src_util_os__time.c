ccb38b309b21203b3a6eab77c7113a1fb49af0dc
François Tigeot <ftigeot@wolfpond.org>
2020-05-18 20:17:04 +0200

    Mesa ports: update to 20.0.7

--- graphics/mesa/files/patch-src_util_os__time.c
+++ graphics/mesa/files/patch-src_util_os__time.c
@@ -2,11 +2,11 @@
 
 --- src/util/os_time.c.orig	2018-04-21 05:48:25 UTC
 +++ src/util/os_time.c
-@@ -95,7 +95,7 @@ os_time_get_nano(void)
+@@ -93,7 +93,7 @@ os_time_get_nano(void)
  void
  os_time_sleep(int64_t usecs)
  {
--#if defined(PIPE_OS_LINUX)
+-#if DETECT_OS_LINUX
 +#if defined(HAVE_CLOCK_NANOSLEEP)
     struct timespec time;
     time.tv_sec = usecs / 1000000;
